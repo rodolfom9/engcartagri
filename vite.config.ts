3ca9fcc9ea7a569f06ee5aad2d8b8d293b445472
rodolfom9 <rodolfo_alto99@hotmail.com>
2025-05-13 16:10:49 -0300

    hum

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -19,13 +18,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './',
+  // 👇 base corrigido para funcionar no GitHub Pages
+  base: mode === "development" ? "/" : "/curricular-flow-builder/",
 }));
